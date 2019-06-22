dad8a889e1708da4bee7561bbc9da24b54d67698
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-22 18:35:52 +0300

    update and add comments

--- data structures/stack/stack.js
+++ data structures/stack/stack.js
@@ -1,27 +1,63 @@
-
+/**
+ * Stacks is a data structure where the last entered data is the first to come out.
+ * Also know as Last-in, First-out (LIFO).
+ *
+ * @description Stack implementation in JavaScript with Array
+ */
 class Stack {
-    constructor() {
-        this.datastore = [];
-        this.top = 0;
-    }
+  /**
+   * @description Create a stack
+   * @param {*} items
+   */
+  constructor(...items) {
+    this.items = [...items];
+  }
 
-    get length() {
-        return this.top;
-    }
+  /**
+   * @description Returns the length of the stack
+   * @returns {number} Number of items in the stack
+   */
+  length() {
+    return this.items.length;
+  }
 
-    push(element) {
-        this.datastore[this.top++] = element;
-    }
+  /**
+   * @description Adds a single or multiple items to the top of the stack
+   * @param {*} items
+   * @returns {number} Number of items in the stack
+   */
+  push(...items) {
+    return this.items.push(...items);
+  }
 
-    pop() {
-        return this.datastore[--this.top];
-    }
+  /**
+   * @description Retrieve & remove the next item of the stack
+   * @returns {*} The top item of the stack
+   */
+  pop() {
+    return this.items.pop();
+  }
 
-    peek() {
-        return this.datastore[this.top - 1];
-    }
+  /**
+   * @description Get the value of the item on top of the stack
+   * @returns {*} The top item of the Stack
+   */
+  peek() {
+    return this.items[this.length() - 1];
+  }
 
-    clear() {
-        this.top = 0;
-    }
-};
+  /**
+   * @description Check if stack is empty
+   * @returns {boolean} True if stack is empty, False otherwise
+   */
+  isEmpty() {
+    return this.length() === 0;
+  }
+
+  /**
+   * @description Removes all the items of the stack
+   */
+  clear() {
+    this.items = [];
+  }
+}
