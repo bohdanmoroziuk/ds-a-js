32943c9b795c27eb2ffe2ab3d7448ee2afb215a7
Bogdan <bogdan7morozyuk@gmail.com>
2019-06-23 11:20:51 +0300

    update and add comments

--- data structures/queue/queue.js
+++ data structures/queue/queue.js
@@ -1,34 +1,64 @@
-
+/**
+ * A queue is a collection in which the entities in the collection are kept in order
+ * and the principal (or only) operations on the collection are the addition of entities
+ * to the rear terminal position, known as enqueue, and removal of entities from the
+ * front terminal position, known as dequeue.
+ * This makes the queue a First-In-First-Out (FIFO) data structure.
+ *
+ * @description Queue implementation in JavaScript with Array.
+ */
 class Queue {
-    constructor() {
-        this.datastore = [];
-    }
+  /**
+   * @description Create a queue.
+   */
+  constructor() {
+    this.items = [];
+  }
 
-    get count() {
-        return this.datastore.length;
-    }
+  /**
+   * Returns current size of queue.
+   * @return {number} Number of items in the queue.
+   */
+  length() {
+    return this.items.length;
+  }
 
-    enqueue(element) {
-        this.datastore.push(element);
-    }
+  /**
+   * Adds a single or multiple items to the queue.
+   * @return {number} Number of items in the queue.
+   */
+  enqueue(...items) {
+    return this.items.push(...items);
+  }
 
-    dequeue() {
-        return this.datastore.shift();
-    }
+  /**
+   * Retrieves & removes the next item of the queue.
+   * @return {*} The data stored in item.
+   */
+  dequeue() {
+    return this.items.shift();
+  }
 
-    front() {
-        return this.datastore[0];
-    }
+  /**
+   * Returns item at front of queue without dequeueing.
+   * @return {*} The data stored in item.
+   */
+  front() {
+    return this.items[0];
+  }
 
-    back() {
-        return this.datastore[this.datastore.length - 1];
-    }
+  /**
+   * @description Checks if queue is empty.
+   * @returns {boolean} False if the queue is empty or True if not.
+   */
+  isEmpty() {
+    return this.length() === 0;
+  }
 
-    empty() {
-        return this.datastore.length === 0;
-    }
-
-    toString() {
-        return this.datastore.join(' ');
-    }
-};
+  /**
+   * @description Removes all the items of the queue.
+   */
+  clear() {
+    this.items = [];
+  }
+}
